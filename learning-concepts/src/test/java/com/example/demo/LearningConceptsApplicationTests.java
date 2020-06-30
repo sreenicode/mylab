57cb10ddb551e6e5fd8492b02ecf2f53e3116f35
sreenivasulukambham <sreenivasulukambham@sreenivsulusair.lan>
2020-06-30 17:27:48 -0500

    test

--- learning-concepts/src/test/java/com/example/demo/LearningConceptsApplicationTests.java
+++ learning-concepts/src/test/java/com/example/demo/LearningConceptsApplicationTests.java
@@ -2,16 +2,19 @@ package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.internal.runners.JUnit4ClassRunner;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@RunWith(JUnit4.class)
 class LearningConceptsApplicationTests {
 
 	@Test
 	void contextLoads() {
 		String nameString="sree";
-		String nameString2="sri";
+		String nameString2="sree";
 		
 		assertEquals(nameString, nameString2);
 	}
